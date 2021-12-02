60761fd905c760a1acdff28f81b051a64a11449f
Stas74 <splotnikov74@gmail.com>
2021-12-01 18:12:37 -0700

    Add more conditionals examples

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,15 @@
-const temperature = -5;
-
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+let temperature = 45;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
 }
 
-console.log("Now you're ready to go outside!");
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+/*
+We read the expression in the above example usually as "if not raining", 
+but it's not always intuitive what "not true" or "not false" 
+mean unless you remember that the ! operator reverses the value of raining. 
+That is, if raining is true, !raining is false. If raining is false, !raining is true.
+*/
